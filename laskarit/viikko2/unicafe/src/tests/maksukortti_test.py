c2f1431002d7518b20de9c1d4f9ee2c2bc10b1b7
wathero <rolf.wathen@gmail.com>
2021-11-15 11:38:25 +0200

    2 testit tehty

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,21 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo,10)
+
+    def test_lataa_oikein(self):
+        self.maksukortti.lataa_rahaa(5)
+        self.assertEqual(self.maksukortti.saldo,15)
+
+    def rahaa_tarpeeksi_ottoon(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(5),True)
+
+    def rahaa_tarpeeksi_ottoon_niin_saldo_vahenee(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(self.maksukortti.saldo,5)
+
+    def rahaa_ei_tarpeeksi_saldo_ei_vahene(self):
+        onko_rahaa=self.maksukortti.ota_rahaa(11)
+        self.assertEqual(self.maksukortti.saldo==10 and onko_rahaa==False,True)
