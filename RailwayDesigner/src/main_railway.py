06a06894b124b0b37f38831ce6908edc256862e5
wathero <rolf.wathen@gmail.com>
2021-11-19 17:02:41 +0200

    Harjoitustyota

--- RailwayDesigner/src/main_railway.py
+++ RailwayDesigner/src/main_railway.py
@@ -1,1 +1,18 @@
-print("Searching fo track...")
+class MapLoader():
+    def __init__(self):
+        self.load_successful=False
+
+    def map_loader(self,map_to_load="defaultmap.jpg"):
+        try:
+            self.load_successful=True
+        except:
+            pass
+def main():
+    print("Train is coming!")
+
+
+
+if __name__ == '__main__':
+    main()
+
+
