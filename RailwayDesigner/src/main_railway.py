89bd9a8b5f6ccfff14eab7cea424b31d61c11fb2
wathero <rolf.wathen@gmail.com>
2021-11-26 20:16:57 +0200

    Hakee kunnat ja piirtää kartan

--- RailwayDesigner/src/main_railway.py
+++ RailwayDesigner/src/main_railway.py
@@ -1,22 +1,21 @@
 import tkinter as tk
 from PIL import ImageTk,Image 
+from location_loader import LocLoader
+from map_maker import MapMaker
 
 
-class MapLoader():
-    def __init__(self):
-        self.load_successful=False
 
-    def map_loader(self,map_to_load="defaultmap.jpg"):
-        try:
-            self.load_successful=True
-        except:
-            pass
 def main():
+    
+    communities=LocLoader().location_loader()
+    map_image,com_in_map=MapMaker().make_map("Helsinki","Tampere",communities)
+    
     window = tk.Tk()
     window.title("Railroad Designer")
-    window.geometry("800x400")
+    window.geometry("800x600")
     
-    img = ImageTk.PhotoImage(Image.open(f"kartta.jpg"))
+    #img = ImageTk.PhotoImage(Image.open(f"./src/kartta.jpg"))
+    img = ImageTk.PhotoImage(map_image)
     imglabel = tk.Label(window, image=img).grid(row=1, column=1)
     window.mainloop()
     print("Train is coming!")
---
+++ RailwayDesigner/src/map_maker.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy as np
+import pandas as pd
+
+class MapMaker():
+    def _init_(self):
+        pass
+    def make_map(self,start_station,end_station,communities):
+
+        start_x=min(communities["Longitude"][start_station],communities["Longitude"][end_station])
+        start_y=min(communities["Latitude"][start_station],communities["Latitude"][end_station])
+        end_x=max(communities["Longitude"][start_station],communities["Longitude"][end_station])
+        end_y=max(communities["Latitude"][start_station],communities["Latitude"][end_station])
+        x_scale=abs(start_x-end_x)
+        y_scale=abs(start_y-end_y)
+        
+        created_map = np.zeros((512,512,3), dtype=np.uint8)
+        for index, row in communities.iterrows():
+            if not start_x<= row["Longitude"]<=end_x:
+                communities.drop([index],axis=0,inplace=True)
+                #print(start_x, row["Longitude"],end_x)
+                continue
+            if not start_y<= row["Latitude"]<=end_y:
+                communities.drop([index],axis=0,inplace=True)
+                
+                continue
+            x=(row["Longitude"]-start_x)/x_scale
+            y=(row["Latitude"]-start_y)/y_scale
+            x_coord=int(100+x*400)
+            y_coord=int(500-y*400)
+            created_map[(x_coord-5):(x_coord+5),(y_coord-5):(y_coord+5),0:2]=100
+            
+        return Image.fromarray(created_map,"RGB"),communities
+
+
+
+
+
+
