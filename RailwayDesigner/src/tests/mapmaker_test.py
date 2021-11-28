8edb0112b1e7e9ebc46f591d1b87b233c0da0aa7
wathero <rolf.wathen@gmail.com>
2021-11-28 22:00:43 +0200

    Töitä vielä testien kanssa

---
+++ RailwayDesigner/src/tests/mapmaker_test.py
@@ -0,0 +1,16 @@
+import unittest
+from map_maker import MapMaker
+from location_loader import LocLoader
+
+class TestLocLoader(unittest.TestCase):
+    def setUp(self):
+        self.loader=LocLoader()
+        self.maker=MapMaker()
+
+    def test_make_map(self):
+        idle_image,pruned_communities=self.maker.make_map\
+            ("Pori","Kotka",self.loader.location_loader())
+        no_image=idle_image is None
+        self.assertEqual(pruned_communities["Latitude"]["Riihimäki"], 60.736737225)
+        self.assertEqual(no_image,False)
+            
