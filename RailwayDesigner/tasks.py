fe5f106c362fc1b5cf9aa2a8c545ad7a00da3902
wathero <rolf.wathen@gmail.com>
2021-11-30 22:29:34 +0200

    Coverage toimii taas

--- RailwayDesigner/tasks.py
+++ RailwayDesigner/tasks.py
@@ -3,3 +3,11 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python src/main_railway.py")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
